0d7c22b6478542cb4f9d118eb2dcc8cb54bdff22
Jose Amesquita <joseamesquita11@gmail.com>
2020-03-09 14:42:43 -0700

    add constructors for input date

---
+++ WeekdayFinder/Models/WeekdayFinder.cs
@@ -0,0 +1,18 @@
+namespace WeekdayFinder.Models
+{
+  public class WeekdayFinder
+  {
+    public int Result { get; set; }
+
+    public int Result2 { get; set; }
+
+    public int Result3 { get; set; }
+
+    public WeekdayFinder(int result, int result2, int result3)
+    {
+      Result = result;
+      Result2 = result2;
+      Result3 = result3;
+    }
+  }
+}
